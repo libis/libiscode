144824cf98171e5dcfc608398a55e1dbedd86114
Naeem Muhammad <naeem.muhammad@libis.kuleuven.be>
2014-07-08 13:20:14 +0000

    preview service

--- contentDeliveryMenu/helpers/previewService.php
+++ contentDeliveryMenu/helpers/previewService.php
@@ -5,27 +5,127 @@
  */
 
 class previewService {
+    private $url_preview;
+    private $url_proxy;
+    private $lido_profile_name;
+    private $lido_provider_name;
 
-    private $urlPreviewMonguz = 'http://euinside.asp.monguz.hu/eck-preview-module/Preview';
-    function getTemplates($provider){
+    # Constructor
+    public function __construct()
+    {
+        $this->loadPreviewConfigurations(dirname(__FILE__).'/config/libiscode.conf');
+    }
 
-        $url = $this->urlPreviewMonguz.'/'.$provider.'/templates';
-//        $url = $this->urlPreviewMonguz.'/'.$provider.'/single/preview/default/';
+    ////Multiple records in a single xml file, preview at once
+    ////Currently this is being used
+    function previewBatchRecords($recordFile){
+
+        $file_path_info = pathinfo($recordFile);
+        $preview_file = $file_path_info['dirname'].'/'.'preview_'.$file_path_info['basename'];
+        $preview_file = str_replace('.xml', '.html', $preview_file);
+        $preview_file = str_replace('edm', '', $preview_file);
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $url)
+                CURLOPT_PROXY => $this->url_proxy,
+                CURLOPT_URL => $this->url_preview.'/'.$this->provider_name.'/'.$this->lido_provider_name.'/single/preview/'.$this->lido_profile_name,
+                CURLOPT_HTTPHEADER => array('Content-Type: application/xml; charset=UTF-8', 'Accept: text/html')
+            )
         );
-        $result = curl_exec($curl);
+
+        curl_setopt($curl,CURLOPT_POSTFIELDS, file_get_contents($recordFile));
+        $response = curl_exec($curl);
         $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         curl_close($curl);
 
         if($responseCode == 200){
-            return $result;
+            file_put_contents($preview_file,print_r($response, true));
+
+            $url = $_SERVER['REQUEST_URI'];
+            $root_dir_path = current(explode('index.php', $url));
+
+            $preview_file_url = 'http://'.$_SERVER['HTTP_HOST'].$root_dir_path.'app'.end(explode('app',$preview_file));
+            $response = '<a  target=\'_blank\'  href="'.$preview_file_url.'">Click</a> to see the preview of '.basename($recordFile).'<br>';
+            return $response;
         }
         else
-            return $responseCode;
+            return 'Error ('.$responseCode. ') in preview service, while previewing '.basename($recordFile).', please try again.<br>';
+
     }
 
+    function previewSingleRecord($record_content){
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_PROXY => $this->url_proxy,
+                CURLOPT_URL => $this->url_preview.'/'.$this->provider_name.'/'.$this->lido_provider_name.'/single/preview/'.$this->lido_profile_name,
+                CURLOPT_HTTPHEADER => array('Content-Type: application/xml; charset=UTF-8', 'Accept: text/html')
+            )
+        );
+
+        curl_setopt($curl,CURLOPT_POSTFIELDS, $record_content);
+        $response = curl_exec($curl);
+        $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if($responseCode == 200)
+            return $response;
+        else
+            return 'Error ('.$responseCode. ') in preview service, please try again.<br>';
+
+    }
+
+    ////Multiple records in a single xml file, each record is extracted from the xml and its preview is get
+    public function previewRecords($recordFile){
+
+        $domDoc = new DOMDocument();
+        $domDoc->formatOutput = true;
+        $domDoc->preserveWhiteSpace = false;
+        $domDoc->load($recordFile);
+        $params = $domDoc->getElementsByTagName('RDF');
+        $counter = 1;
+        $record_preview ="";
+        foreach ($params as $param) {
+
+            $param_id = $param->getElementsByTagName('identifier');
+            $value = ($param_id->length > 0) ? $param_id->item(0)->nodeValue : '';
+            $record_number = 'Record Number '.$counter.'. Identifier:'.$value.'<br>';
+
+            $newDoc = new DOMDocument;
+            $rdfNode = $newDoc->importNode($param, true);
+            $newDoc->appendChild($rdfNode);
+            $record_content = $newDoc->saveXML();
+            $record_preview .=$record_number.'<br>'.$this->previewSingleRecord($record_content).'<br>';
+            $newDoc = null;
+
+            $counter++;
+        }
+
+        $file_path_info = pathinfo($recordFile);
+        $preview_file = $file_path_info['dirname'].'/'.'preview_'.$file_path_info['basename'];
+        $preview_file = str_replace('.xml', '.html', $preview_file);
+        $preview_file = str_replace('edm', '', $preview_file);         //should be replaced for edm
+
+        file_put_contents($preview_file,$record_preview."\n");
+        $url = $_SERVER['REQUEST_URI'];
+        $root_dir_path = current(explode('index.php', $url));
+
+        $preview_file_url = 'http://'.$_SERVER['HTTP_HOST'].$root_dir_path.'app'.end(explode('app',$preview_file));
+        $response = '<a  target=\'_blank\'  href="'.$preview_file_url.'">Click</a> to see the preview of '.basename($recordFile).'<br>';
+        return $response;
+
+
+    }
+
+    public function loadPreviewConfigurations($conf_file_path){
+        $o_config = Configuration::load($conf_file_path);
+
+        $this->url_preview = $o_config->get('url_preview');
+        $this->url_proxy = $o_config->get('url_proxy');
+        $this->lido_profile_name = $o_config->get('lidoProfileName');
+        $this->lido_provider_name = $o_config->get('lidoProvider');
+    }
 
 }
--- contentDeliveryMenu/views/preview_html.php
+++ contentDeliveryMenu/views/preview_html.php
@@ -1,17 +1,70 @@
 <?php
     require_once(__CA_BASE_DIR__."/app/plugins/contentDeliveryMenu/helpers/previewService.php");
 
-
 	echo _t("<h1>Libis Content Delivery System</h1>\n");
 	echo _t("<h2>Preview Service</h2>\n");
 
 
 
     $previewService = new previewService;
+    $filesDirectory = __CA_BASE_DIR__.'/app/plugins/contentDeliveryMenu/files';
+    $searchString = 'edm';
 
-    echo "<center><br><b>"."Preview"."</b><br></center>";
-    echo "<br><b>"."Preview Record"."</b><br>";
-    echo "Response :".$previewService -> getTemplates('libis');
+    $it = new RecursiveDirectoryIterator($filesDirectory);
+    $display = Array ( 'xml' );
+
+    echo '<div>';
+    echo "<table border='0' width='100%' style='border-width: 1px;
+                                                   border-color:#000000; border-style: solid;'>";
+    $formAction = dirname($_SERVER['SCRIPT_NAME'])."/index.php/contentDeliveryMenu/ContentDelivery/Index/universe/Preview";
+    echo "<form action='$formAction' method='post'>";
+    foreach(new RecursiveIteratorIterator($it) as $file)
+    {
+        if (in_array(strtolower(array_pop(explode('.', $file))), $display)){
+            if (strpos($file,$searchString) !== false) {
+                echo "<tr style='text-align: center; border: 1px'>";
+                echo '<td style="text-align: left; width:1%;">';
+
+                echo "<input type='checkbox' value=' . $file .' name='previewFiles[]'>";
+
+                echo '</td>';
+
+                echo '<td style="text-align: left">';
+                echo '<a target = "_blank" style = "text-decoration:none;" href='.dirname($_SERVER['SCRIPT_NAME']).
+                    "/app/plugins/contentDeliveryMenu/files".end(explode('files', $file)).'>'
+                    .basename($file);
+                echo '</a>';
+
+                echo '</td>';
+                echo "</tr>";
+            }
+        }
+    }
+
+    echo "<tr style='text-align: center'>";
+    echo "<td align='center' colspan='2'> <input type='submit' value='Preview Records' name='recordPreview'> </td>";
+    echo "</tr>";
+    echo '</form>';
+    echo "</table>";
+    echo '<br>';
+    echo '</div>';
+
+    if(isset($_POST['previewFiles'])){
+        echo 'Preview Results<br>';
+        echo '<br>';
+        foreach($_POST['previewFiles'] as $item){
+            $previewFileLocation = explode('files', $item);
+
+            if(isset($previewFileLocation[1])){
+                $previewFile = $filesDirectory.trim(str_replace('\\', '/', $previewFileLocation[1]));
+                $previewFile = trim(trim($previewFile, '.'));
+                $response = $previewService->previewBatchRecords($previewFile);
+                echo $response;
+                echo '<br>';
+            }
+        }
+    }
+
 
 ?>
 
