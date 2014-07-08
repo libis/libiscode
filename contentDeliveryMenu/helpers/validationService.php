cf1e6005a47b3e1159171b8c0ae68cd4f766c4b6
Naeem Muhammad <naeem.muhammad@libis.kuleuven.be>
2014-07-08 13:24:06 +0000

    validation service

--- contentDeliveryMenu/helpers/validationService.php
+++ contentDeliveryMenu/helpers/validationService.php
@@ -6,76 +6,79 @@
  * Time: 13:01
  */
 
+require_once(__CA_LIB_DIR__.'/vendor/autoload.php');
+
 class validationService {
-    private $urlValidationMonguz = 'http://euinside.asp.monguz.hu/eck-validation-servlet/validation';
-    private $monguzHost = 'euinside.asp.monguz.hu';
 
-    private  $urlValidationSemnatika = 'http://euinside.semantika.si';
-    private  $semantikaHost = 'euinside.semantika.si';
+    private $url_proxy;
+    private $profile_name;
+    private $provider_name;
+    private $url_validation;
+    private $validation_host;
+    private $url_validation_alternate;
+    private $validation_alternate_host;
 
-    function validateRecords($recordFile, $provider, $requestTitle, $profileName){
+
+    # Constructor
+    public function __construct()
+    {
+        $this->loadValidationConfigurations(dirname(__FILE__).'/config/libiscode.conf');
+    }
+
+    function validateRecords($recordFile){
+
         $fields = array(
-            'Name'          => $requestTitle,
-            'record'        => '@'.$recordFile,
-            'provider'      => $provider,
-            'profileName'   => $profileName
+            'record' => '@'.$recordFile
         );
+
         $curl = curl_init();
         curl_setopt_array($curl, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $this->urlValidationMonguz)
-        );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-                'Host:'.$this->monguzHost,
-                'Content-Type: 	multipart/form-data')
+                CURLOPT_PROXY => $this->url_proxy,
+                CURLOPT_URL => $this->url_validation.'/'.$this->provider_name.'/single/validate/'.$this->profile_name,
+                CURLOPT_HTTPHEADER, array('Content-type: application/xml')
+            )
         );
         curl_setopt($curl,CURLOPT_POSTFIELDS, $fields);
+        $response = curl_exec($curl);
 
-        $result = curl_exec($curl);
-        $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        if($responseCode != 200)
-            return $responseCode;
-        else
-            return $result;
-
+        return array('response_code' => curl_getinfo($curl, CURLINFO_HTTP_CODE), 'response_body' => json_decode($response, true));
     }
 
-    function validateRecordsSemantika($recordFile, $provider, $requestTitle, $profileName){
+    function validateRecordsInBatch($recordFile){
 
         $fields = array(
-            'Name'          => $requestTitle,
-            'xmldoc'        => '@'.$recordFile,
-            'XmlDocument'      => '@'.$recordFile
-//            'Source'   => $profileName
+            'record' => '@'.$recordFile
         );
 
-        $requestParameter = json_encode($fields);
-
+        $set_name = $this->provider_name.round(microtime(true) * 1000);
         $curl = curl_init();
         curl_setopt_array($curl, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $this->urlValidationSemnatika.'/validation/validate')
+                CURLOPT_PROXY => $this->url_proxy,
+                CURLOPT_URL => $this->url_validation.'/'.$this->provider_name.'/'.$set_name.'/validate/'.$this->profile_name,
+                CURLOPT_HTTPHEADER, array('Content-type: application/xml')
+            )
         );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-                'Host: localhost:13987',
-                'User-Agent: Fiddler',
-                'Content-Type: application/json; charset=utf-8')
-        );
-        curl_setopt($curl,CURLOPT_POSTFIELDS, $requestParameter);
+        curl_setopt($curl,CURLOPT_POSTFIELDS, $fields);
+        $response = curl_exec($curl);
 
-        $result = curl_exec($curl);
-        $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        return $responseCode;
-
-//        if($responseCode != 200)
-//            return $responseCode;
-//        else
-//            return $result;
-
+        return array('response_code' => curl_getinfo($curl, CURLINFO_HTTP_CODE), 'response_body' => json_decode($response, true));
     }
 
 
+    public function loadValidationConfigurations($conf_file_path){
+        $o_config = Configuration::load($conf_file_path);
 
+        $this->url_validation = $o_config->get('url_validation');
+        $this->url_validation_alternate = $o_config->get('url_validation_alternate');
+        $this->validation_host = $o_config->get('validation_host');
+        $this->validation_alternate_host = $o_config->get('validation_alternate_host');
+        $this->url_proxy = $o_config->get('url_proxy');
+
+        $this->profile_name = $o_config->get('profileName');
+        $this->provider_name = $o_config->get('provider');
+
+    }
 
 }
